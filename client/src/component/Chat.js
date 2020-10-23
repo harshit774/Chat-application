b2e9d73d3ddeea4d8e6cbaa8eea4c5172d6b1b0b
ritikpy <ritikvarshney2014@gmail.com>
2020-10-23 21:41:40 +0530

    Basic boilerplate setUp

---
+++ client/src/component/Chat.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Chat = ()=> {
+    return (
+        <h1>Chat the club</h1>
+    )
+};
+export default Chat;
---
+++ client/src/component/Join.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Join = ()=> {
+    return (
+        <h1>Join the club</h1>
+    )
+};
+export default Join;
