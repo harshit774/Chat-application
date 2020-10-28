b4b3983b27ccd644cdb45f7604cc2bb9621c90fd
ritikpy <ritikvarshney2014@gmail.com>
2020-10-28 21:50:27 +0530

    socket.io with client-side

--- client/src/component/Chat/Chat.jsx
+++ client/src/component/Chat/Chat.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+import './chat.css'
 
-const Chat = ()=> {
+let socket;
+
+const Chat = ({ location })=> {
+    const [ name, setName ] = useState('');
+    const [ room, setRoom ] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(()=> {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, (error) => {
+            if(error){
+                alert(error);
+            }
+        });
+
+    }, [ENDPOINT, location.search]);
     return (
         <h1>Chat the club</h1>
     )
--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
 const router = require('./route');
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+app.use(router);
+
 
 //Socket.io conection and disconnection
 io.on('connection', socket => {
     console.log("New User entered");
+
+    socket.on('join', ({ name, room })=>{
+        console.log(name,room);
+    })
     socket.on('disconnect', ()=>{
         console.log("User had left");
-    });
+    })
 });
-app.use(router);
 
-app.listen( PORT, () => console.log(`Server is running on port ${5000}`));
+
+server.listen( PORT, () => console.log(`Server is running on port ${PORT}`));
